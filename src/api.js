1f4808f0dc38a83b53652c49a1b9ab0af26ec3d7
NAVEEN M <202109648+naveen-murugan-1@users.noreply.github.com>
2026-03-29 14:26:49 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api' || 'https://myportfolio-nine-iota-33.vercel.app/api',
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api' || 'https://my-portfolio-dun-nine-30.vercel.app/api',
 });
 
 // Add a request interceptor
